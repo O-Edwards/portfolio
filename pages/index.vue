f5765d855a9499eda4a8cb1503d01299c1d25bea
Edwards <oedwards@travelers.com>
2019-01-10 11:39:48 -0500

    Added SCSS, Made Edits to hero image, mocked out future divs

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,19 @@
 <template lang="pug">
   div
-    //- b-navbar(toggleable="md" type="dark" variant="info" sticky=true)
+    b-navbar(toggleable="md" type="dark" variant="info" sticky=true id="nav")
       b-navbar-toggle(target="nav_collapse")
       b-navbar-brand(href="#") Oluwatosin Edwards
       b-collapse(is-nav id="nav_collapse")
         b-navbar-nav
           b-nav-item(href="#") Home
           b-nav-item(href="#") Menu
-    .alignment__container
+    .top
+      .illustration
       .header__text
-        //- h1 Hi! Welcome to My Portfolio Site
-      .container__hero
-        img#space(src="./assets/portfolio-hero.svg")
-    .container
-      logo
-      h1.title
-        ac-petersen
-      h2.subtitle
-        | Site for My Portfolio
-      div.links
-        a.button--green(href="https://nuxtjs.org/" target="_blank") Documentation
-        a.button--grey(href="https://github.com/nuxt/nuxt.js" target="_blank") GitHub
-
+        h1.title portfolio
+        h2.subtitle Welcome to my portfolio
+    .portfolio
+    .contact
 </template>
 
 <script>
@@ -30,52 +22,52 @@ import Logo from '~/components/Logo.vue'
 export default {
   components: {
     Logo
+  },
+  computed: {
+  },
+  methods: {
+
   }
 }
 </script>
 
-<style>
-#space{
-  top: 0px;
-  right: 0px;
-  position: absolute;
+<style <style lang="scss" scoped>
+@media(max-width: 1200px){
+  .illustration {
+      width: 700px;
+      background-size: 700px;
+  }
 }
-.alignment__container {
+
+.top{ 
   display: flex;
   flex-direction: row;
-}
-.container {
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.container__hero {
-  
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  .illustration {
+    width: 820px;
+    height: 667px;
+    position: absolute;
+    right: 0;
+    top: 0;
+    background: url(./assets/portfolio-hero.svg) no-repeat;
+    z-index: 2;
+  }
+  .header__text{
+    .title {
+      font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+        'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+      display: block;
+      font-weight: 300;
+      font-size: 100px;
+      color: #35495e;
+      letter-spacing: 1px;
+    }
+    .subtitle {
+      font-weight: 300;
+      font-size: 42px;
+      color: #526488;
+      word-spacing: 5px;
+      padding-bottom: 15px;
+    }
+  }
 }
 </style>
