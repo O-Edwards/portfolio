af328e44b5c481e2df274118d7fac40803925d3b
Edwards <oedwards@travelers.com>
2019-01-13 10:23:33 -0500

    Prelim for landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,23 @@
 <template lang="pug">
   div
-    b-navbar(toggleable="md" type="dark" variant="info" sticky=true id="nav")
-      b-navbar-toggle(target="nav_collapse")
-      b-navbar-brand(href="#") Oluwatosin Edwards
-      b-collapse(is-nav id="nav_collapse")
-        b-navbar-nav
-          b-nav-item(href="#") Home
-          b-nav-item(href="#") Menu
+    //- b-navbar(toggleable="md" type="dark" variant="info" sticky=true id="nav")
+    //-   b-navbar-toggle(target="nav_collapse")
+    //-   b-navbar-brand(href="#") Oluwatosin Edwards
+    //-   b-collapse(is-nav id="nav_collapse")
+    //-     b-navbar-nav
+    //-       b-nav-item(href="#") Home
+    //-       b-nav-item(href="#") Menu
     .top
-      .illustration
+      .landing__image
       .header__text
-        h1.title portfolio
-        h2.subtitle Welcome to my portfolio
-    .portfolio
-    .contact
+        .space__man
+          img#man(src="./assets/moon-man.png")
+        h1.title Welcome
+        img#circles(src="./assets/circles.svg")
+        h2.subtitle My name is Oluwatosin Edwards, and I am a Software Engineer.<br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed dox <br>tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis <br>nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        button#contact Contact Me
+    .middle
+    
 </template>
 
 <script>
@@ -32,41 +36,75 @@ export default {
 </script>
 
 <style <style lang="scss" scoped>
-@media(max-width: 1200px){
-  .illustration {
-      width: 700px;
-      background-size: 700px;
-  }
-}
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,900');
 
 .top{ 
   display: flex;
   flex-direction: row;
-  .illustration {
-    width: 820px;
-    height: 667px;
+  .landing__image {
     position: absolute;
     right: 0;
     top: 0;
-    background: url(./assets/portfolio-hero.svg) no-repeat;
-    z-index: 2;
+    background: url(./assets/landing-image.svg) no-repeat;
+    width: 70vw;
+    height: 50vw;
   }
   .header__text{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding-top: 10vh;
+    padding-left: 5rem;
+    .space__man{ 
+      background: url(./assets/moon.png) no-repeat;
+      width: 15vw;
+      height: 30vh;
+      display: flex;
+      align-items: center;
+       #man {
+          width: 10vw;
+          top: -10vh;
+          position: relative;
+          -webkit-animation: mover 1s infinite  alternate;
+          animation: mover 1s infinite  alternate;
+        }
+        @-webkit-keyframes mover {
+          0% { transform: translateY(0); }
+          100% { transform: translateY(-10px); }
+        }
+        @keyframes mover {
+          0% { transform: translateY(0); }
+          100% { transform: translateY(-10px); }
+        }
+    }
     .title {
-      font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-        'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+      font-family: 'Roboto', sans-serif;
       display: block;
-      font-weight: 300;
-      font-size: 100px;
-      color: #35495e;
+      font-weight: 600;
+      font-size: 5rem;
+      color: #4AA2FF;
       letter-spacing: 1px;
+      margin: 0;
+    }
+    #circles{
+      width: 8vw;
+      padding: 2rem 0rem 2rem 0rem;
     }
     .subtitle {
       font-weight: 300;
-      font-size: 42px;
+      font-size: 30px;
       color: #526488;
-      word-spacing: 5px;
       padding-bottom: 15px;
+    }
+    #contact {
+      background: url(./assets/button.svg) no-repeat;
+      width: 12rem;
+      height: 4rem;
+      border: none;
+      font-family: Roboto-Bold;
+      font-size: 22px;
+      color: #FFFFFF;
+      text-align: center;
     }
   }
 }
