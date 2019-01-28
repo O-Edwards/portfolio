a40ef0fd6fd8800d4ea2bf5aad8e89f5ea8d4436
Edwards <oedwards@travelers.com>
2019-01-28 08:56:19 -0500

    Changed the landing image

--- pages/index.vue
+++ pages/index.vue
@@ -2,56 +2,64 @@
   div
     .top
       .landing__image
-      .header__text
-        .space__man
-          img#man(src="./assets/moon-man.png")
-        h1.title Welcome
-        img#circles(src="./assets/circles.svg")
-        h2.subtitle My name is Oluwatosin Edwards, and I am a Software Engineer.<br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed dox <br>tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis <br>nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        button#contact Contact Me
-    .middle
+        .title 
+          h5#name I'm Oluwatosin Edwards
+          h3#title I am a <br />Software Engineer
+          a(href="#middle") <a>-</a> About Me
+    .middle#middle
       .colored__container
         h3 Hi, My name is Oluwatosin Edwards
         p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed dox <br>tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis <br>nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        b-card#skills-card(title="Skills"
-          tag="article"
-          style="text-align:center;"
-          class="mb-2")
-          .card-left
-            .card__icon
-              i(class="fas fa-laptop" style="color:#6A2B7F")
-            .card__description
-              p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed dox <br>tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis <br>nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            .card__list
-              ul
-                li HTML
-                li CSS
-                li JavaScript
-                li Vue JS
-          //- .card-right
-          //-   .card__icon
-          //-     i(class="fas fa-igloo" style="color:#6A2B7F")
-          //-   .card__description
-          //-     p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed dox <br>tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis <br>nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          //-   .card__list
-          //-     ul
-          //-       li HTML
-          //-       li CSS
-    
     .middle-2
+      b-card#skills-card(title="Skills"
+        tag="article"
+        style="text-align:center;"
+        class="mb-2")
+        .card-left
+          .card__icon
+            i(class="fas fa-laptop" style="color:#6A2B7F")
+          .card__description
+            p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed dox <br>tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis <br>nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          .card__list
+            ul
+              li HTML
+              li CSS
+              li JavaScript
+              li Vue JS
       h3 Projects
       p Here you can view some of my recent projects
+      .project-list
+        b-card(overlay
+            img-src="https://www.idealtoy.co.za/assets/app/public/images/magento/coming_soon.jpg"
+            img-alt="Card Image"
+            text-variant="white"
+            )
+        b-card(overlay
+            img-src="https://www.idealtoy.co.za/assets/app/public/images/magento/coming_soon.jpg"
+            img-alt="Card Image"
+            text-variant="white"
+            )
+        b-card(overlay
+            img-src="https://www.idealtoy.co.za/assets/app/public/images/magento/coming_soon.jpg"
+            img-alt="Card Image"
+            text-variant="white"
+            )
     .bottom
+      b-card#contact-card(
+        tag="article"
+        style="text-align:center;"
+        class="mb-2")
+          h4 Contact methods
+          p Hey hit me up
+          b-button Contact
     
 
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
   components: {
-    Logo
   },
   computed: {
   },
@@ -62,7 +70,7 @@ export default {
 </script>
 
 <style <style lang="scss" scoped>
-@import url('https://fonts.googleapis.com/css?family=Roboto:400,900');
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600');
 @import url('https://use.fontawesome.com/releases/v5.6.3/css/all.css');
 @import "assets/variables";
 
@@ -70,80 +78,52 @@ export default {
   display: flex;
   flex-direction: row;
   .landing__image {
-    position: absolute;
-    right: 0;
-    top: 0;
     background: url(./assets/landing-image.svg) no-repeat;
-    width: 70vw;
-    height: 50vw;
-  }
-  .header__text{
+    background-size: cover;
+    width: 100vw;
+    height: 100vh;
     display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    padding-top: 10vh;
-    padding-left: 5rem;
-    .space__man{ 
-      background: url(./assets/moon.png) no-repeat;
-      width: 15vw;
-      height: 30vh;
-      display: flex;
-      align-items: center;
-      #man {
-          width: 10vw;
-          top: -10vh;
-          position: relative;
-          -webkit-animation: mover 1s infinite  alternate;
-          animation: mover 1s infinite  alternate;
+    flex-direction: row;
+    align-items: center;
+    .title{
+      margin-left: 12rem;
+      h5 {
+        font-family: 'Open Sans', sans-serif;
+        color: white;
+        font-size: 2rem;
+        padding-bottom: 2rem;
+      }
+      h3 {
+        font-family: 'Open Sans', sans-serif;
+        display: block;
+        font-weight: 600;
+        font-size: 4rem;
+        color: #fff;
+        letter-spacing: 1px; 
+        padding-bottom: 2rem;
+      }
+      a {
+        color: #fff;
+        a {
+          &:hover{
+            text-decoration: none;
+          }
+          color: #F68630;
+          text-decoration: none;
         }
-        @-webkit-keyframes mover {
-          0% { transform: translateY(0); }
-          100% { transform: translateY(-10px); }
-        }
-        @keyframes mover {
-          0% { transform: translateY(0); }
-          100% { transform: translateY(-10px); }
-        }
+      }
     }
-    .title {
-      font-family: 'Roboto', sans-serif;
-      display: block;
-      font-weight: 600;
-      font-size: 5rem;
-      color: #4AA2FF;
-      letter-spacing: 1px;
-      margin: 0;
-    }
-    #circles{
-      width: 8vw;
-      padding: 2rem 0rem 2rem 0rem;
-    }
-    .subtitle {
-      font-weight: 300;
-      font-size: 30px;
-      color: #526488;
-      padding-bottom: 3rem;
-    }
-    #contact {
-      background: url(./assets/button.svg) no-repeat;
-      width: 12rem;
-      height: 4rem;
-      border: none;
-      font-family: Roboto-Bold;
-      font-size: 22px;
-      color: #FFFFFF;
-      text-align: center;
-      outline: none;
+      
     }
   }
-}
+
 
 .middle{
   .colored__container{
     background-image: linear-gradient(to bottom right,$purp, $pink);
     width: 100vw;
-    height: 35rem;
-    margin: 10rem 0 0 0;
+    height: 20rem;
+    margin: 0 0 0 0;
     display: flex;
     flex-direction: column;
     color: #FFFFFF;
@@ -156,7 +136,15 @@ export default {
     p {
       text-align: center;
     }
-    #skills-card {
+  }
+}
+
+.middle-2 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 3rem;
+  #skills-card {
       display: flex;
       flex-direction: row;
       background-color: #fff;;
@@ -164,6 +152,7 @@ export default {
       width:80vw;
       height: fit-content;
       box-shadow: 0 0 6px 0 rgba(70,73,77,0.30);
+      margin: -6rem 0 4rem 0 !important;
       .card-left{
         // border-right-style: solid;
         display: flex;
@@ -184,11 +173,34 @@ export default {
         align-items: center;
       }
     }
+  .project-list{
+    & > *{
+      width: 33.3%;
+      margin: 0 2rem 2rem 2rem;
+    }
+    display: flex;
+    flex-direction: row;
   }
 }
 
 .bottom {
+  display: flex;
+  justify-content: center;
+  color: #fff;
+  #contact-card {
+    margin-top: -2rem;
+    display: flex;
+    justify-content: center;
+    width: 60vw;
+    max-height: 4.5rem;
+    background-color: #293347;
+    .card-body{
+      display: flex;
+      flex-direction: row;
+      justify-content: space-evenly;
+    }
+  }
    background-image: linear-gradient(to bottom right,$purp, $pink);
-   height: 10rem;
+   height: 15rem;
 }
 </style>
